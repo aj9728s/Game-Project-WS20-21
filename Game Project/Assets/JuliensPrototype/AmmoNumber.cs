583b2eedbd720afff3fab7f2e64f2587c7f87415
Julien Ahn <julien.ahn@skynet.be>
2020-10-30 22:27:05 +0100

    Prototyp_1.4_Julien_FlashlightBattery

--- Game Project/Assets/JuliensPrototype/AmmoNumber.cs
+++ Game Project/Assets/JuliensPrototype/AmmoNumber.cs
@@ -1,16 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
-public class AmmoNumber : MonoBehaviour
+public class WeaponAttributLayout : MonoBehaviour
 {
     [SerializeField]
-    private SOAmmoManager ammoManager;
+    private SOAmmoManager weaponManager;
 
-    // Update is called once per frame
+    [SerializeField]
+    private Sprite[] images;
+
     void Update()
     {
-        GetComponent<TextMeshProUGUI>().text = ammoManager.ammoAmount.ToString();
+        if(weaponManager.selectedWeapon == 0)
+        {
+            GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+            GetComponentInChildren<Image>().color = new Color(0, 0, 0, 0);
+            GetComponentInChildren<Image>().sprite = null;
+        }
+
+        else if (weaponManager.selectedWeapon == 1)
+        {
+            GetComponentInChildren<Image>().color = new Color(255, 255, 255, 255);
+            GetComponentInChildren<TextMeshProUGUI>().enabled = true;
+            GetComponentInChildren<TextMeshProUGUI>().text = weaponManager.ammoAmount.ToString();
+            GetComponentInChildren<Image>().sprite = images[0];
+        }
+
+        else if (weaponManager.selectedWeapon == 2)
+        {
+            GetComponentInChildren<Image>().color = new Color(255, 255, 255, 255);
+            GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+            if(weaponManager.batteryLevel == 4)
+                GetComponentInChildren<Image>().sprite = images[1];
+            if (weaponManager.batteryLevel == 3)
+                GetComponentInChildren<Image>().sprite = images[2];
+            if (weaponManager.batteryLevel == 2)
+                GetComponentInChildren<Image>().sprite = images[3];
+            if (weaponManager.batteryLevel == 1)
+                GetComponentInChildren<Image>().sprite = images[4];
+            if (weaponManager.batteryLevel == 0)
+                GetComponentInChildren<Image>().sprite = images[5];
+        }
+      
     }
 }
--- Game Project/Assets/JuliensPrototype/SOAmmoManager.cs
+++ Game Project/Assets/JuliensPrototype/SOAmmoManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class SOAmmoManager : ScriptableObject
 {
     public int ammoAmount;
+    public int batteryLevel;
     public List<int> weapons;
     public int selectedWeapon;
 
