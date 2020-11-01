07322444e57ebd9c4b4d82c875a568e238dd929b
Julien Ahn <julien.ahn@skynet.be>
2020-11-01 03:41:43 +0100

    Prototyp_1.7.2_final
    Dialogue, BugFixes, AddedWeapons

---
+++ Game Project/Assets/JuliensPrototype/KeyOpen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyOpen : MonoBehaviour
+{
+    [SerializeField]
+    private int weaponNR = 0;
+
+    [SerializeField]
+    private SOAmmoManager weaponManager;
+
+    [SerializeField]
+    private GameObject door;
+
+    private float dist;
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(this.GetComponentInParent<Transform>().position, door.transform.position);
+
+        if (weaponManager.selectedWeapon == weaponNR && dist <= 2 && Input.GetButtonDown("Fire1"))
+        {
+            Destroy(door);
+            weaponManager.selectedWeapon = weaponManager.weapons[0];
+            weaponManager.weapons.Remove(weaponNR);
+        }
+    }
+}
---
+++ Game Project/Assets/JuliensPrototype/Prisoner.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Prisoner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private string weaponHint;
+
+    [SerializeField]
+    private int weaponNR;
+
+    [SerializeField]
+    private SOAmmoManager command;
+
+    [SerializeField]
+    private UnityEvent dialogueTriggerlvl1;
+
+    private bool gotItem = false;
+
+    private bool fPressed = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, player.transform.position) <= 2)
+        {
+            if (!fPressed)
+            {
+                command.command = "F";
+                command.timer2 = 0.1f;
+            }
+            
+            if (Input.GetKey(KeyCode.F))
+            {
+                fPressed = true;
+                dialogueTriggerlvl1.Invoke();
+            }
+
+        }
+    }
+
+    public void endDialogue()
+    {
+        fPressed = false;
+
+        if (!gotItem)
+        {
+            gotItem = true;
+            command.textHint = weaponHint;
+            command.timerText = 4f;
+            command.weapons.Add(weaponNR);
+            command.selectedWeapon = weaponNR;
+
+        }
+      
+
+    }
+    public void getWeapon()
+    {
+       
+        if (!gotItem)
+        {
+            gotItem = true;
+            command.textHint = weaponHint;
+            command.timerText = 4f;
+            command.weapons.Add(weaponNR);
+            command.selectedWeapon = weaponNR;
+
+        }
+
+
+    }
+}
