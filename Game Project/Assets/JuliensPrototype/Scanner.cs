76fa3897b6e8f7fc187badcc763d99db36834c5c
Julien Ahn <julien.ahn@skynet.be>
2020-11-01 15:56:16 +0100

    TestArea_KnifeScannerDropSystem_1.0
    Mit dem Scanner sollen gewisse Items in Leveln/Bereichen verboten werden

---
+++ Game Project/Assets/JuliensPrototype/Scanner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class Scanner : MonoBehaviour
+{
+    [SerializeField]
+    private int[] weaponNr;
+
+    [SerializeField]
+    private SOAmmoManager weapons;
+
+    [SerializeField]
+    private UnityEvent playerDied;
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < weaponNr.Length; i++)
+        {
+            if (other.CompareTag("Player") && weapons.weapons.Contains(weaponNr[i]))
+                playerDied.Invoke();
+        }
+           
+
+    }
+}
