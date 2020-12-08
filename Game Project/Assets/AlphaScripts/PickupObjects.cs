a9dad83eff577188d46e74b36d1926d9201dbac5
Julien Ahn <julien.ahn@skynet.be>
2020-12-08 01:57:05 +0100

    canvas_1.0 flashlight_1.0

--- Game Project/Assets/AlphaScripts/PickupObjects.cs
+++ Game Project/Assets/AlphaScripts/PickupObjects.cs
@@ -33,8 +33,8 @@ public class PickupObjects : MonoBehaviour
     [SerializeField]
     public int weaponNR;
 
-    [SerializeField]
-    private GameObject textHintLayout;
+    //[SerializeField]
+    //private GameObject textHintLayout;
 
     [SerializeField]
     private string weaponHint;
