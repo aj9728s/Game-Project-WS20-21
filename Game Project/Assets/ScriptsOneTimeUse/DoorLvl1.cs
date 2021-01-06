25499c302e46ed45438adf1142d746bb389d6956
Julien Ahn <julien.ahn@skynet.be>
2021-01-06 16:50:11 +0100

    lvl1_transition

--- Game Project/Assets/ScriptsOneTimeUse/DoorLvl1.cs
+++ Game Project/Assets/ScriptsOneTimeUse/DoorLvl1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DoorLvl1 : MonoBehaviour
 {
@@ -12,10 +13,24 @@ public class DoorLvl1 : MonoBehaviour
     private AudioClip explosion;
 
     [SerializeField]
+    private AudioClip tinnitus;
+
+    [SerializeField]
     private Transform CameraPosition;
 
     [SerializeField]
     private float timeSzeneSwap;
+
+    [SerializeField]
+    private Image panel;
+
+    private bool explosionBool = false;
+
+    void Update()
+    {  
+        if(explosionBool)
+            panel.color = new Color(panel.color.r, panel.color.g, panel.color.b, panel.color.a + 0.01f); 
+    }
 
     public void enableHint()
     {
@@ -25,14 +40,21 @@ public class DoorLvl1 : MonoBehaviour
     public void gotKnocked()
     {
         AudioSource.PlayClipAtPoint(knock, CameraPosition.position, 2f);
+        
         StartCoroutine(changeSzene());
     }
 
     IEnumerator changeSzene()
     {
         yield return new WaitForSeconds(timeSzeneSwap);
-        AudioSource.PlayClipAtPoint(explosion, CameraPosition.position, 2f);
+        AudioSource.PlayClipAtPoint(explosion, CameraPosition.position, 4f);
         yield return new WaitForSeconds(1);
+        AudioSource.PlayClipAtPoint(tinnitus, CameraPosition.position, 4f);
+        explosionBool = true;
+
+
+
+        yield return new WaitForSeconds(3);
         SceneManager.LoadScene(2);
     }
 }
