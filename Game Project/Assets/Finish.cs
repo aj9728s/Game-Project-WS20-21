6ded1c39d104dba33c38eb64ba3e4354e85ec15a
Renngurke <49644690+Renngurke@users.noreply.github.com>
2021-03-05 21:49:49 +0100

    LVL4
    LVL4 komplett

---
+++ Game Project/Assets/Finish.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Finish : MonoBehaviour
+{
+    public GameObject finishScreenUI;
+
+
+    public void HitFinish()
+    {
+
+        finishScreenUI.SetActive(true);
+        Time.timeScale = 0f;
+
+
+    }
+    public void ZumHauptmenü()
+    {
+
+        finishScreenUI.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+}
