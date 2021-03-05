372a941c44b16f5129a0936c814d91e5e51ba221
Julien Ahn <julien.ahn@skynet.be>
2021-03-05 23:42:37 +0100

    lastVBersion

---
+++ Game Project/Assets/LastLevelScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastLevelScript : MonoBehaviour
+{
+    [SerializeField]
+    private SOLvLManager lvlManager;
+
+    [SerializeField]
+    private SOWeaponManager weaponManager;
+
+    // Start is called before the first frame update
+    public void trigger()
+    {
+        lvlManager.absolviertesLVL = 0;
+        weaponManager.ammoAmount = 0;
+        weaponManager.weapons.Clear();
+        weaponManager.selectedWeapon = 0;
+    }
+
+   
+}
