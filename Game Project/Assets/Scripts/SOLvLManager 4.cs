71d9a6e0770cc7b1172802b77115e300f2429c1d
Renngurke <49644690+Renngurke@users.noreply.github.com>
2021-03-05 23:02:08 +0100

    Credits in 4

---
+++ Game Project/Assets/Scripts/SOLvLManager 4.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SOLvLManager4 : MonoBehaviour
+{
+    [SerializeField]
+    private SOLvLManager4 lvLManager4;
+
+    [SerializeField]
+    private SOWeaponManager weaponManager;
+
+   
+    public void HitFinishReset()
+    {
+        weaponManager.ammoAmount = 0;
+        weaponManager.batteryLevel = 4;
+        weaponManager.weapons.Clear();
+        weaponManager.selectedWeapon = 0;
+  
+    }
+}
